1942238d16bc6b46569f73aee4ad116bf190d907
hamza-ga <hhamzagamal@gmail.com>
2022-08-20 14:25:03 +0200

    finish the part that i made for void project until basic user apis with some error handling

--- blog-app-apis/src/main/java/com/blog/exeptions/GlobalExeptionHandler.java
+++ blog-app-apis/src/main/java/com/blog/exeptions/GlobalExeptionHandler.java
@@ -1,6 +1,19 @@
 package com.blog.exeptions;
 
 
+import com.blog.payloads.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@RestControllerAdvice
 public class GlobalExeptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundExeption.class)
+    public ResponseEntity<ApiResponse> responseNotFoundExceptionHandler(ResourceNotFoundExeption ex){
+        String message = ex.getMessage();
+        ApiResponse apiResponse = new ApiResponse(message ,false);
+        return new ResponseEntity<>(apiResponse, HttpStatus.NOT_FOUND);
+    }
 }
